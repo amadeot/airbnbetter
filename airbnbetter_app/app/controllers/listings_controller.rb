3f43d8283536ce06b1d2f028f076b2bf72eca1b6
Chris Johnson <johnsonchris98@gmail.com>
2015-12-15 11:20:03 -0500

    Updated Models, Views, Controllers, and Route.rb

---
+++ airbnbetter_app/app/controllers/listings_controller.rb
@@ -0,0 +1,34 @@
+class ListingsController < ApplicationController::Base
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
+end
+
+
+
+# listings GET    /listings(.:format)          listings#index
+#              POST   /listings(.:format)          listings#create
+#  new_listing GET    /listings/new(.:format)      listings#new
+# edit_listing GET    /listings/:id/edit(.:format) listings#edit
+#      listing GET    /listings/:id(.:format)      listings#show
+#              PATCH  /listings/:id(.:format)      listings#update
+#              PUT    /listings/:id(.:format)      listings#update
+#              DELETE /listings/:id(.:format)      listings#destroy
+
+
--- airbnbetter_app/app/models/user.rb
+++ airbnbetter_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_secure_password
   has_many :listings
   validates :email, uniqueness: true, presence: true
 end
