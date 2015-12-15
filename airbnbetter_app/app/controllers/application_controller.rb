bf7c962247e3c0997d9c83b6709650388ee92992
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-15 14:04:12 -0500

    added current_user to application controller

--- airbnbetter_app/app/controllers/application_controller.rb
+++ airbnbetter_app/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # set the current_user instance variable if we have a session
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # This allows us to call curernt_user in our views
+  helper_method :current_user
+
 end
